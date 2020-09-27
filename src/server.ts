3dcd87d3d387dcea1d08bab3c22979fc520a074d
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-09-27 12:38:25 +0100

    feat: added expirationComplete event

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,10 @@ export * from './events/subjects';
 
 export * from './events/implementations/ticket/ticketCreatedEvent';
 export * from './events/implementations/ticket/ticketUpdatedEvent';
+
 export * from './events/implementations/order/orderCancelledEvent';
 export * from './events/implementations/order/orderCreatedEvent';
+
+export * from './events/implementations/expiration/expirationCompleteEvent';
 
 export * from './events/types/orderStatus';
